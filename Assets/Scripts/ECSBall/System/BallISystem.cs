e2f428f1d6275b6976b80a35fd7f43d3056ea452
MacStudio <qin.batista@outlook.com>
2022-11-11 23:51:26 -0800

    able to create

--- Assets/Scripts/ECSBall/System/BallISystem.cs
+++ Assets/Scripts/ECSBall/System/BallISystem.cs
@@ -5,11 +5,12 @@ using Unity.Entities;
 using Unity.Jobs;
 
 [BurstCompile]
-public partial struct BallSystem : ISystem
+public partial struct BallISystem : ISystem
 {
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
+
     }
     [BurstCompile]
     public void OnDestroy(ref SystemState state)
@@ -18,19 +19,20 @@ public partial struct BallSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)//work as main thread
     {
-        // JobHandle jobHandle = new BallJob
-        // {
-        //     deltaTime = SystemAPI.Time.DeltaTime
-        // }.ScheduleParallel(state.Dependency);
-        // jobHandle.Complete();
+        float deltaTime = SystemAPI.Time.DeltaTime;
+        JobHandle jobHandle = new BallJob
+        {
+            _deltaTime = deltaTime,
+        }.ScheduleParallel(state.Dependency);
+        jobHandle.Complete();
     }
 }
 [BurstCompile]
 public partial struct BallJob : IJobEntity
 {
-    public float deltaTime;
+    public float _deltaTime;
     public void Execute(BallAspect _ballAspect)
     {
-        _ballAspect.FlyingAround(deltaTime);
+        _ballAspect.FlyingAround(_deltaTime);
     }
 }
--- Assets/Scripts/ECSSpawner/SpawnerSystem.cs
+++ Assets/Scripts/ECSSpawner/SpawnerSystem.cs
@@ -6,20 +6,20 @@ public partial class SpawnerSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        // EntityQuery playerEntityQuery = GetEntityQuery(typeof(BallTagComponent));
-        // SpawnerComponent spawnerComponent = SystemAPI.GetSingleton<SpawnerComponent>();
-        // EntityCommandBuffer entityCommandBuffer = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(World.Unmanaged);
-        // if (playerEntityQuery.CalculateEntityCount() < 2000)
-        // {
-        //     Entity spawnedEntity = entityCommandBuffer.Instantiate(spawnerComponent._ballEntity);
-        //     entityCommandBuffer.SetComponent(spawnedEntity,
-        //     new BallComponent
-        //     {
-        //         _position = new float3(10, 10, 10),
-        //         _acceleration = new float3(1, 2, 3),
-        //         _direction = new float3(1, 2, 3),
-        //         _random = new Unity.Mathematics.Random(1),
-        //     });
-        // }
+        EntityQuery playerEntityQuery = GetEntityQuery(typeof(BallTagComponent));
+        SpawnerComponent spawnerComponent = SystemAPI.GetSingleton<SpawnerComponent>();
+        EntityCommandBuffer entityCommandBuffer = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(World.Unmanaged);
+        if (playerEntityQuery.CalculateEntityCount() < 2000)
+        {
+            Entity spawnedEntity = entityCommandBuffer.Instantiate(spawnerComponent._ballEntity);
+            entityCommandBuffer.SetComponent(spawnedEntity,
+            new BallComponent
+            {
+                _position = new float3(10, 10, 10),
+                _acceleration = new float3(1, 2, 3),
+                _direction = new float3(1, 2, 3),
+                _random = new Unity.Mathematics.Random(1),
+            });
+        }
     }
 }
